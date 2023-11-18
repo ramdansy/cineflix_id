b0c4f8a4aaa3e9d674bd3f2cf69e64d76beb6291
UsernameAnda <ramdansyaripudin@gmail.com>
2023-11-18 22:37:44 +0700

    update firebase auth & repository

--- lib/data/firebase/firebase_user_repository.dart
+++ lib/data/firebase/firebase_user_repository.dart
@@ -4,6 +4,8 @@ import 'package:cineflix_id/data/repositories/user_repository.dart';
 import 'package:cineflix_id/domain/entities/result/result.dart';
 import 'package:cineflix_id/domain/entities/user/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart';
 
 class FirebaseUserRepository implements UserRepository {
   final FirebaseFirestore _firebaseFirestore;
@@ -15,15 +17,32 @@ class FirebaseUserRepository implements UserRepository {
   Future<Result<User>> createUser(
       {required String uid,
       required String email,
-      required String password,
+      required String name,
       String? photoUrl,
-      int balance = 0}) {
-    // TODO: implement createUser
-    throw UnimplementedError();
+      int balance = 0}) async {
+    CollectionReference<Map<String, dynamic>> users =
+        _firebaseFirestore.collection('users');
+
+    await users.doc(uid).set({
+      'uid': uid,
+      'email': email,
+      'name': name,
+      'photoUrl': photoUrl,
+      'balance': balance,
+    });
+
+    DocumentSnapshot<Map<String, dynamic>> result = await users.doc(uid).get();
+
+    if (result.exists) {
+      return Result.success(User.fromJson(result.data()!));
+    } else {
+      return const Result.failed("Failed to create user data");
+    }
   }
 
   @override
   Future<Result<User>> getUser({required String uid}) async {
+    //for get user
     DocumentReference<Map<String, dynamic>> documentReference =
         _firebaseFirestore.doc('users/$uid');
 
@@ -38,28 +57,96 @@ class FirebaseUserRepository implements UserRepository {
   }
 
   @override
-  Future<Result<int>> getUserBalance({required String uid}) {
-    // TODO: implement getUserBalance
-    throw UnimplementedError();
+  Future<Result<int>> getUserBalance({required String uid}) async {
+    DocumentReference<Map<String, dynamic>> documentReference =
+        _firebaseFirestore.doc('users/$uid');
+    DocumentSnapshot<Map<String, dynamic>> result =
+        await documentReference.get();
+
+    if (result.exists) {
+      return Result.success(result.data()!['balance']);
+    } else {
+      return const Result.failed('User not found');
+    }
   }
 
   @override
-  Future<Result<User>> updateUser({required String user}) {
-    // TODO: implement updateUser
-    throw UnimplementedError();
+  Future<Result<User>> updateUser({required User user}) async {
+    try {
+      DocumentReference<Map<String, dynamic>> documentReference =
+          _firebaseFirestore.doc('users/${user.uid}');
+      await documentReference.update(user.toJson());
+
+      DocumentSnapshot<Map<String, dynamic>> result =
+          await documentReference.get();
+
+      if (result.exists) {
+        User updateUser = User.fromJson(result.data()!);
+
+        if (updateUser == user) {
+          return Result.success(updateUser);
+        } else {
+          return const Result.failed("Failed to update user data");
+        }
+      } else {
+        return const Result.failed("Failed to update user data");
+      }
+    } on FirebaseException catch (e) {
+      return Result.failed(e.message ?? "Failed to update user data");
+    }
   }
 
   @override
   Future<Result<User>> updateUserBalance(
-      {required String uid, required int balance}) {
-    // TODO: implement updateUserBalance
-    throw UnimplementedError();
+      {required String uid, required int balance}) async {
+    DocumentReference<Map<String, dynamic>> documentReference =
+        _firebaseFirestore.doc('users/$uid');
+    DocumentSnapshot<Map<String, dynamic>> result =
+        await documentReference.get();
+
+    if (result.exists) {
+      await documentReference.update({'balance': balance});
+
+      DocumentSnapshot<Map<String, dynamic>> updateResult =
+          await documentReference.get();
+
+      if (updateResult.exists) {
+        User updateUser = User.fromJson(updateResult.data()!);
+        if (updateUser.balance == balance) {
+          return Result.success(updateUser);
+        } else {
+          return const Result.failed('Failed to updated user balance');
+        }
+      } else {
+        return const Result.failed('Failed to retrieve updated user balance');
+      }
+    } else {
+      return const Result.failed('User not found');
+    }
   }
 
   @override
   Future<Result<User>> uploadProfilePicture(
-      {required User user, required File imageFile}) {
-    // TODO: implement uploadProfilePicture
-    throw UnimplementedError();
+      {required User user, required File imageFile}) async {
+    String filename = basename(imageFile.path);
+
+    Reference reference = FirebaseStorage.instance.ref().child(filename);
+
+    try {
+      await reference.putFile(imageFile);
+
+      String downloadUrl = await reference.getDownloadURL();
+
+      var updateResult =
+          await updateUser(user: user.copyWith(photoUrl: downloadUrl));
+
+      if (updateResult.isSuccess) {
+        return Result.success(updateResult.resultValue!);
+      } else {
+        return Result.failed(updateResult.errorMessage!);
+      }
+    } catch (e) {
+      return const Result.failed("Failed to upload profile picture");
+    }
   }
 }
--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -7,13 +7,13 @@ abstract interface class UserRepository {
   Future<Result<User>> createUser({
     required String uid,
     required String email,
-    required String password,
+    required String name,
     String? photoUrl,
     int balance = 0,
   });
 
   Future<Result<User>> getUser({required String uid});
-  Future<Result<User>> updateUser({required String user});
+  Future<Result<User>> updateUser({required User user});
   Future<Result<int>> getUserBalance({required String uid});
   Future<Result<User>> updateUserBalance(
       {required String uid, required int balance});
