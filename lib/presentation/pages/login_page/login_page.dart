9fe026f7545842eb737fdbf2586af13e4689b657
UsernameAnda <ramdansyaripudin@gmail.com>
2023-12-03 22:17:45 +0700

    add login page

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -1,37 +1,107 @@
-import 'package:cineflix_id/presentation/misc/extensions/build_context_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../misc/extensions/build_context_extension.dart';
+import '../../misc/methods.dart';
 import '../../providers/router/router_provider.dart';
 import '../../providers/user_data/user_data_provider.dart';
+import '../../widgets/cnflx_text_field.dart';
 
 class LoginPage extends ConsumerWidget {
-  const LoginPage({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    ref.listen(userDataProvider, (previous, next) {
-      if (next is AsyncData) {
-        if (next.value != null) {
-          ref.read(routerProvider).goNamed('main');
+    ref.listen(
+      userDataProvider,
+      (previous, next) {
+        if (next is AsyncData) {
+          if (next.value != null) {
+            ref.read(routerProvider).goNamed('main');
+          }
+        } else if (next is AsyncError) {
+          context.showSnackBar(next.error.toString());
         }
-      } else if (next is AsyncError) {
-        context.showSnackBar(next.error.toString());
-      }
-    });
+      },
+    );
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              ref
-                  .read(userDataProvider.notifier)
-                  .login(email: 'ramdan@yopmail.com', password: '123456');
-            },
-            child: const Text('Login')),
+      body: ListView(
+        children: [
+          verticalSpace(100),
+          const Center(
+            child: Text(
+              'CINEFLIX.ID',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+          ),
+          verticalSpace(100),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              children: [
+                CineflixTextField(
+                  labelText: 'Email',
+                  controller: emailController,
+                ),
+                verticalSpace(24),
+                CineflixTextField(
+                  labelText: 'Password',
+                  controller: passwordController,
+                  obscureText: true,
+                ),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Forgot password?',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    )),
+                verticalSpace(24),
+                switch (ref.watch(userDataProvider)) {
+                  AsyncData(:final value) => value == null
+                      ? SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                ref.read(userDataProvider.notifier).login(
+                                    email: emailController.text,
+                                    password: passwordController.text);
+                              },
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )),
+                        )
+                      : const Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                  _ => const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                },
+                verticalSpace(24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account? "),
+                    TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Register here',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
